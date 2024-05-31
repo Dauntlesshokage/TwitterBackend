6af78ddb09974f5fa7b4f347b6e10f172a4a3054
Ananth Lakshman Kumar <ananthhockage@gmail.com>
2024-05-31 18:35:07 -0400

    Initial commit with CRUD endpoints setup

---
+++ src/routes/userRoutes.ts
@@ -0,0 +1,27 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.get("/", (req, res) => {
+  res.status(501).json({ error: "Not implememented" });
+});
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  res.status(501).json({ error: `Not implemented : ${id}` });
+});
+
+router.post("/", (req, res) => {
+  res.status(501).json({ error: "Not implemented" });
+});
+
+router.put("/:id", (req, res) => {
+  const { id } = req.params;
+  res.status(501).json({ error: `Not implemented : ${id}` });
+});
+
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  res.status(501).json({ error: `Not implemented : ${id}` });
+});
+
+export default router;
